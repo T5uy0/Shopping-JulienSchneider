ff093fb6f6fb06a019150dfb42f27d7c7a0f77cc
Julien Schneider <julienschneider964@gmail.com>
2025-02-08 16:42:01 +0100

    feat: Validation of all test

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -1,3 +1,9 @@
+/**
+ * @file      CartItem.js
+ * @brief     Class of CartItem.
+ * @author    Created by Julien.Schneider
+ * @version   07-02-2025 - original (dedicated to RIA1)
+ */
 "use strict";
 
 const InvalidArticleIdException = require("./InvalidArticleIdException.js");
@@ -7,65 +13,62 @@ const InvalidPriceException = require("./InvalidPriceException.js");
 module.exports = class CartItem {
 
     //region private attributes
-    articleId;
-    name;
-    quantity;
-    price;
+    #articleId;
+    #name;
+    #quantity;
+    #price;
     //endregion private attributes
 
     //region public methods
     constructor(articleId, name, quantity, price) {
-        //Invalid quantity (smaller than 1)
-        if (quantity < 1) {
-            throw new InvalidQuantityException();
-        }
-        //Invalid price (smaller than 10)
-        if (price < 10) {
-            throw new InvalidPriceException();
-        }
-        //Invalid article id (smaller than 1)
-        if (articleId < 1) {
-            throw new InvalidArticleIdException();
-        }
-
-        this.articleId = articleId;
-        this.name = name;
-        this.quantity = quantity;
-        this.price = price;
+        if (articleId < 1)
+            throw new InvalidArticleIdException;
+        if (quantity < 1)
+            throw new InvalidQuantityException;
+        if (price < 10)
+            throw new InvalidPriceException;
+        this.#articleId = articleId;
+        this.#name = name;
+        this.#quantity = quantity;
+        this.#price = price
     }
 
     get articleId() {
-        throw new Error();
+        return this.#articleId;
     }
 
     get name() {
-        throw new Error();
+        return this.#name;
     }
 
     get quantity() {
-        throw new Error();
+        return this.#quantity;
     }
 
     set quantity(value) {
-        throw new Error();
+        if (value < 1) {
+            throw new InvalidQuantityException;
+        }
+        this.#quantity = value;
     }
 
     get price() {
-        throw new Error();
+        return this.#price;
     }
 
-    set price(price) {
-        throw new Error();
+    set price(value) {
+        if (value < 10) {
+            throw new InvalidPriceException;
+        }
+        this.#price = value;
     }
 
     get total() {
-        throw new Error();
+        return this.#quantity * this.#price;
     }
+
     //endregion public methods
 
     //region private methods
     //endregion private methods
 }
-
-
-
