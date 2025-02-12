8c3e0cf24e2f1f54cb0ddfdf6df0dd61ce09fa3b
Julien Schneider <julienschneider964@gmail.com>
2025-02-12 11:16:12 +0100

    feat: create Cart,test items_NominalCase_GetItems passed

---
+++ src/Cart/Cart.js
@@ -0,0 +1,28 @@
+/**
+ * @file      Cart.js
+ * @brief     Class of Cart.
+ * @author    Created by Julien.Schneider
+ * @version   12-02-2025
+ */
+"use strict";
+const EmptyCartException = require("./EmptyCartException.js");
+const UpdateCartException = require("./UpdateCartException.js");
+
+
+module.exports = class Cart {
+
+    //region private attributes
+    #items
+
+    //endregion private attributes
+
+    //region public methods
+    constructor(items = []) {
+        this.items = items;
+    }
+
+    //endregion public methods
+
+    //region private methods
+    //endregion private methods
+}
