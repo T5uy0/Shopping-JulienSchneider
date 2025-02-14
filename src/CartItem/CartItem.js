a71159ad8ae4ec7d4111c2ef56d5344ea4fc5565
Julien Schneider <julienschneider964@gmail.com>
2025-02-14 11:00:54 +0100

    fix: delete uncall getter name

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -37,10 +37,6 @@ module.exports = class CartItem {
         return this.#articleId;
     }
 
-    get name() {
-        return this.#name;
-    }
-
     get quantity() {
         return this.#quantity;
     }
